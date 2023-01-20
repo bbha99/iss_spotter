332e99d561012dd56cbeac492b0a14df08a13f77
bbha99 <branha99@gmail.com>
2023-01-20 01:17:18 -0800

    getting ISS pasttimes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -10,11 +10,21 @@ const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned IP:' , ip);
 // });
 
-fetchCoordsByIP("70.79.164.103", (error, coordinates) => {
+// fetchCoordsByIP("70.79.164.103", (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+
+fetchISSFlyOverTimes({ latitude: 49.2827291, longitude: -123.1207375 }, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned the next upcoming times that the ISS will fly over the given coordinates, and the duration it will remain "visible" in the sky:' , passTimes);
 });
